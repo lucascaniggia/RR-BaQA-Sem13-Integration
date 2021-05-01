4366daa643faace4df1eb3ce91317f02cde2fb3f
Lucas Caniggia <lucascaniggia5@gmail.com>
2021-05-01 15:12:52 -0300

    checkEmail and calculateArea tests added

--- complexOperations.test.js
+++ complexOperations.test.js
@@ -4,13 +4,37 @@ import * as complexOperations from './complexOperations';
 describe('complexOperation - Unit Tests', () => {
   describe('checkEmail', () => {
     it('first test for checkEmail', () => {
-      
+      expect(complexOperations.checkEmail('narellacalvente@gmail.com')).toStrictEqual('The email is valid');
+    });
+
+    it('second test for checkEmail', () => {
+      expect(complexOperations.checkEmail(23)).toStrictEqual('The email should be an string');
+    });
+
+    it('third test for checkEmail', () => {
+      expect(complexOperations.checkEmail('Lucas Caniggia')).toStrictEqual('The email is invalid');
+    });
+
+    it('fourth -undefined- test for checkEmail', () => {
+      expect(complexOperations.checkEmail(undefined)).toStrictEqual('The email is invalid');
     });
   });
 
   describe('calculateArea', () => {
-    it('first test for calculateArea', () => {
+    it('square test for calculateArea', () => {
+      expect(complexOperations.calculateArea('square', 2, 3)).toBe(6);
+    });
 
+    it('rectangle test for calculateArea', () => {
+      expect(complexOperations.calculateArea('rectangle', 2, 4)).toBe(8);
+    });
+
+    it('triangle test for calculateArea', () => {
+      expect(complexOperations.calculateArea('triangle', 2, 4)).toBe(4);
+    });
+
+    it('circle test for calculateArea', () => {
+      expect(complexOperations.calculateArea('circle', 8)).toBeCloseTo(201.06);
     });
   });
 
@@ -34,7 +58,7 @@ describe('complexOperation - Unit Tests', () => {
 
   describe('numberOfOddAndEvenNumbers', () => {
     it('first test for numberOfOddAndEvenNumbers', () => {
-      
+
     });
   });
 });
